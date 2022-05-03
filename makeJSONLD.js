54df936832610d213fcc533a9c3a93325d03f096
Taisuke Fukuno <fukuno@jig.jp>
2022-05-04 07:27:40 +0900

    simplify jsonld

--- makeJSONLD.js
+++ makeJSONLD.js
@@ -2,14 +2,23 @@ import { CSV } from "https://js.sabae.cc/CSV.js";
 
 class JSONLD {
   static make(vocab, data) {
+    const context = (() => {
+      if (vocab.url) {
+        const context = {};
+        context[vocab.prefix] = vocab.url;
+        return context;
+      } else {
+        return vocab;
+      }
+    })();
+    const prefix = vocab.prefix ? vocab.prefix + ":" : "";
+
     const list = [];
     for (const d of data) {
-      const context = {};
-      context[vocab.prefix] = vocab.url;
       const ld = { "@context": context };
       for (const name in d) {
         if (d[name] !== "") {
-          ld[vocab.prefix + ":" + name] = d[name];
+          ld[prefix + name] = d[name];
         }
       }
       list.push(ld);
@@ -20,7 +29,8 @@ class JSONLD {
 
 const data = CSV.toJSON(await CSV.fetch("data-latest/event-latest.csv"));
 
-const vocab = { prefix: "brevent", url: "https://registry-catalog.registries.digital.go.jp/dataset/?groups=g1-000304#" };
+//const vocab = { prefix: "brevent", url: "https://registry-catalog.registries.digital.go.jp/dataset/?groups=g1-000304#" };
+const vocab = "event-schema.jsonld";
 const jsonld = JSONLD.make(vocab, data);
 console.log(jsonld);
 
